ac4cb354deb5e6f7e99aa7efe56caa19dfcd58a2
devcrema <devcrema@gmail.com>
2021-07-26 01:33:21 +0900

    :recycle: refactoring zsh plugin install scripts

--- mac/setting_zsh.sh
+++ mac/setting_zsh.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s "$(which zsh)"
-echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
-echo 'source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
-sed -i 's/^plugins=(git)/plugins=(git zsh-z)/g' ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/agkozak/zsh-z "$ZSH_CUSTOM/plugins/zsh-z"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-completions zsh-z zsh-syntax-highlighting)/g' ~/.zshrc
