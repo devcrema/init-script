e959c7256d0175d3d62d425b43a14ad083db1c36
devcrema <devcrema@gmail.com>
2021-07-26 15:25:44 +0900

    :hammer: add vscode plugins

--- mac/setting_ide.sh
+++ mac/setting_ide.sh
@@ -21,3 +21,9 @@ chmod +x $CHARM
 
 # code
 echo "export PATH=\"\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin\"" >> ~/.zshrc
+export ~/.zshrc
+code --install-extension xyz.local-history
+code --install-extension pkief.material-icon-theme
+code --install-extension ms-python.python
+code --install-extension streetsidesoftware.code-spell-checker
+cp -f ./mac/backup/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
