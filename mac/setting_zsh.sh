aba0bce7068a4ef37f14e99768330a6cc1842513
devcrema <devcrema@gmail.com>
2021-07-27 23:47:00 +0900

    :recycle: refactor codes

--- mac/setting_zsh.sh
+++ mac/setting_zsh.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s "$(which zsh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/agkozak/zsh-z "$ZSH_CUSTOM/plugins/zsh-z"
-git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+ZSH_PLUGIN_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGIN_PATH/zsh-autosuggestions"
+sudo git clone https://github.com/agkozak/zsh-z "$ZSH_PLUGIN_PATH/zsh-z"
+sudo git clone https://github.com/zsh-users/zsh-completions "$ZSH_PLUGIN_PATH/zsh-completions"
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGIN_PATH/zsh-syntax-highlighting"
 
 sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-completions zsh-z zsh-syntax-highlighting)/g' ~/.zshrc
