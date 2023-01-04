48d810a4532a40289168c64653d2185ae255784d
Park SungEun <devcrema@gmail.com>
2023-01-05 02:30:23 +0900

    Update setting_personal_zshrc.sh

--- mac/setting_personal_zshrc.sh
+++ mac/setting_personal_zshrc.sh
@@ -7,7 +7,7 @@
 
   echo "alias dnew='git clone https://devcrema@github.com/devcrema/development_note.git $HOME/dd && code $HOME/dd'"
   echo "alias dclear='rm -rf $HOME/dd'"
-  echo "alias dcommit='git add . && git status && gitmoji -c && git push origin master & git push origin2 master & git push origin3 master'"
+  echo "alias dcommit='git pull && git push origin master & git push origin2 master & git push origin3 master && git add . && git status && gitmoji -c && git push origin master & git push origin2 master & git push origin3 master'"
   echo "alias dpush='git push origin master & git push origin2 master & git push origin3 master'"
   echo "alias dpushf='git push --force origin master & git push --force origin2 master & git push --force origin3 master'"
 
